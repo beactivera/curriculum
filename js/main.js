ce9d725c8341fbe769417b2f63bd73beac1c544c
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2018-02-20 23:09:52 +0100

    add interactive navigation

--- js/main.js
+++ js/main.js
@@ -1,20 +1,179 @@
-//let admBtn = document.querySelectorAll('.adm-elem');
-//
-//admBtn.forEach(setupClick)
-//function setupClick(admBtn){
-//    admBtn.addEventListener('click', btnClicked);
-//}
-//
-//function btnClicked(e){
-//    e.target.nextElementSibling.classList.toggle('hide');
-//}
-//
-//function btnClicked(e){
-//    console.log(e.target.nextSibling)
-//    let modal = e.target.nextSibling;
-//    modal.style.display = "block"
-//}
+/////// interactive nav   ////////
 
+let introBtn = document.getElementById('intro-nav');
+let programBtn = document.getElementById('program-nav');
+let admBtn = document.getElementById('adm-nav');
+let abroadBtn = document.getElementById('abroad-nav');
+let studentBtn = document.getElementById('student-nav');
+let careerBtn = document.getElementById('career-nav');
+let contactBtn = document.getElementById('contact-nav');
+
+//introduction
+introBtn.addEventListener('click', showIntro)
+
+function showIntro(){
+    // highlight buttom
+    introBtn.classList.add('active-color');
+    programBtn.classList.remove('active-color');
+    admBtn.classList.remove('active-color');
+    abroadBtn.classList.remove('active-color');
+    studentBtn.classList.remove('active-color');
+    careerBtn.classList.remove('active-color'); 
+    contactBtn.classList.remove('active-color');
+    //change sites
+    document.getElementById("front").classList.add('hide');
+    document.getElementById("introduction").classList.remove('hide');
+    document.getElementById("program").classList.add('hide');
+    document.getElementById("admission").classList.add('hide');
+    document.getElementById("abroad").classList.add('hide');
+    document.getElementById("student").classList.add('hide');
+    document.getElementById("career").classList.add('hide');
+    document.getElementById("contact").classList.add('hide');
+}
+
+//study program
+programBtn.addEventListener('click', showProgram)
+
+function showProgram(){
+    // highlight buttom
+    introBtn.classList.remove('active-color');
+    programBtn.classList.add('active-color');
+    admBtn.classList.remove('active-color');
+    abroadBtn.classList.remove('active-color');
+    studentBtn.classList.remove('active-color');
+    careerBtn.classList.remove('active-color'); 
+    contactBtn.classList.remove('active-color');
+    //change sites
+    document.getElementById("front").classList.add('hide');
+    document.getElementById("introduction").classList.add('hide');
+    document.getElementById("program").classList.remove('hide');
+    document.getElementById("admission").classList.add('hide');
+    document.getElementById("abroad").classList.add('hide');
+    document.getElementById("student").classList.add('hide');
+    document.getElementById("career").classList.add('hide');
+    document.getElementById("contact").classList.add('hide');
+}
+
+//admission
+admBtn.addEventListener('click', showAdmission)
+
+function showAdmission(){
+    // highlight buttom
+    introBtn.classList.remove('active-color');
+    programBtn.classList.remove('active-color');
+    admBtn.classList.add('active-color');
+    abroadBtn.classList.remove('active-color');
+    studentBtn.classList.remove('active-color');
+    careerBtn.classList.remove('active-color'); 
+    contactBtn.classList.remove('active-color');
+    //change sites
+    document.getElementById("front").classList.add('hide');
+    document.getElementById("introduction").classList.add('hide');
+    document.getElementById("program").classList.add('hide');
+    document.getElementById("admission").classList.remove('hide');
+    document.getElementById("abroad").classList.add('hide');
+    document.getElementById("student").classList.add('hide');
+    document.getElementById("career").classList.add('hide');
+    document.getElementById("contact").classList.add('hide');
+}
+
+//abroad
+abroadBtn.addEventListener('click', showAbroad)
+
+function showAbroad(){
+    // highlight buttom
+    introBtn.classList.remove('active-color');
+    programBtn.classList.remove('active-color');
+    admBtn.classList.remove('active-color');
+    abroadBtn.classList.add('active-color');
+    studentBtn.classList.remove('active-color');
+    careerBtn.classList.remove('active-color'); 
+    contactBtn.classList.remove('active-color');
+    //change sites
+    document.getElementById("front").classList.add('hide');
+    document.getElementById("introduction").classList.add('hide');
+    document.getElementById("program").classList.add('hide');
+    document.getElementById("admission").classList.add('hide');
+    document.getElementById("abroad").classList.remove('hide');
+    document.getElementById("student").classList.add('hide');
+    document.getElementById("career").classList.add('hide');
+    document.getElementById("contact").classList.add('hide');
+}
+
+//student life
+studentBtn.addEventListener('click', showStudent)
+
+function showStudent(){
+    // highlight buttom
+    introBtn.classList.remove('active-color');
+    programBtn.classList.remove('active-color');
+    admBtn.classList.remove('active-color');
+    abroadBtn.classList.remove('active-color');
+    studentBtn.classList.add('active-color');
+    careerBtn.classList.remove('active-color'); 
+    contactBtn.classList.remove('active-color');
+    //change sites
+    document.getElementById("front").classList.add('hide');
+    document.getElementById("introduction").classList.add('hide');
+    document.getElementById("program").classList.add('hide');
+    document.getElementById("admission").classList.add('hide');
+    document.getElementById("abroad").classList.add('hide');
+    document.getElementById("student").classList.remove('hide');
+    document.getElementById("career").classList.add('hide');
+    document.getElementById("contact").classList.add('hide');
+}
+
+//career
+careerBtn.addEventListener('click', showCareer)
+
+function showCareer(){
+    // highlight buttom
+    introBtn.classList.remove('active-color');
+    programBtn.classList.remove('active-color');
+    admBtn.classList.remove('active-color');
+    abroadBtn.classList.remove('active-color');
+    studentBtn.classList.remove('active-color');
+    careerBtn.classList.add('active-color'); 
+    contactBtn.classList.remove('active-color');
+    //change sites
+    document.getElementById("front").classList.add('hide');
+    document.getElementById("introduction").classList.add('hide');
+    document.getElementById("program").classList.add('hide');
+    document.getElementById("admission").classList.add('hide');
+    document.getElementById("abroad").classList.add('hide');
+    document.getElementById("student").classList.add('hide');
+    document.getElementById("career").classList.remove('hide');
+    document.getElementById("contact").classList.add('hide');
+}
+
+//contact
+contactBtn.addEventListener('click', showContact)
+
+function showContact(){
+    // highlight buttom
+    introBtn.classList.remove('active-color');
+    programBtn.classList.remove('active-color');
+    admBtn.classList.remove('active-color');
+    abroadBtn.classList.remove('active-color');
+    studentBtn.classList.remove('active-color');
+    careerBtn.classList.remove('active-color'); 
+    contactBtn.classList.add('active-color');
+    //change sites
+    document.getElementById("front").classList.add('hide');
+    document.getElementById("introduction").classList.add('hide');
+    document.getElementById("program").classList.add('hide');
+    document.getElementById("admission").classList.add('hide');
+    document.getElementById("abroad").classList.add('hide');
+    document.getElementById("student").classList.add('hide');
+    document.getElementById("career").classList.add('hide');
+    document.getElementById("contact").classList.remove('hide');
+}
+
+
+/////// modals plus json ////////
+
+
+/////// admission buttoms ///////
 
 let admBtn1 = document.querySelector('.adm-elem1');
 admBtn1.addEventListener('click', showMoreAdm1);
@@ -94,3 +253,21 @@ function showMoreAdm10(){
     admBtn10.nextElementSibling.classList.toggle('hide');
     admBtn10.classList.toggle('change-color');
 }
+
+//let admBtn = document.querySelectorAll('.adm-elem');
+//
+//admBtn.forEach(setupClick)
+//function setupClick(admBtn){
+//    admBtn.addEventListener('click', btnClicked);
+//}
+//
+//function btnClicked(e){
+//    e.target.nextElementSibling.classList.toggle('hide');
+//}
+//
+//function btnClicked(e){
+//    console.log(e.target.nextSibling)
+//    let modal = e.target.nextSibling;
+//    modal.style.display = "block"
+//}
+
